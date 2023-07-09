19390b7957200510df6dd3ad231d08e941a24ffa
Steven Wu <swu121@gmail.com>
2023-07-09 16:06:15 -0700

    updated frontend api

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export default axios.create({
-    baseURL: "http://localhost:5000/api/v1/restaurants", 
+    baseURL: "http://yelpclone-env.eba-zma3vudm.us-east-1.elasticbeanstalk.com/api/v1/restaurants", 
     headers: {
         "Content-type": "application/json"
     }
